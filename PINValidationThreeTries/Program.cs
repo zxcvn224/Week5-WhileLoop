b1d7b864c1198b8c26eec6dc283a6846e97a4cf6
Opilane TTHK <opilane@tthk.ee>
2021-09-29 09:47:01 +0300

    Week5-WhileLoop

---
+++ PINValidationThreeTries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PINValidationThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int cpurandom = rnd.Next(1, 11);
+            int i = 0;
+            while (i < 3)
+            {
+                Console.WriteLine("Sisesta number");
+                int Userguess = Convert.ToInt32(Console.ReadLine());
+                if (cpurandom == Userguess)
+                {
+                    Console.WriteLine("Tubli! Oled mängu võitnud");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Vale number, {3 - i} Proovi alles");
+                    i++; //i = i + 1;
+
+                }
+            }
+            if (i == 3)
+            {
+                Console.WriteLine("Oled mängu kaotanud");
+            }
+        }
+    }
+}
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada PIN-koodi
+            //program võrdleb sisestatud PIN-koodi arvuga 1234
+            //kui sisestatud PIN-kood on 1234
+            //programm kuvab konsoolis "Tere tulemast!"
+            //kui sisestatud PIN on vale, programm kuvab konsoolis 
+            //"Vale PIN. Proovi uuesti"
+            //*katsete arv on piiramatu
+
+            //boolean --> true/false
+            bool LoopActive = true;
+            while (LoopActive)
+            {
+                Console.WriteLine("Sisesta PIN:");
+                int userPIN = Convert.ToInt32(Console.ReadLine());
+
+                if(userPIN == 1234)
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    LoopActive = false;
+                }
+            }
+            
+            Console.WriteLine("kena päeva!");
+        }
+    }
+}
